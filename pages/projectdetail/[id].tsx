718bd76e657f361c22cb2ed652f0bc07bc1febda
InhooPark <park.linhoo@gmail.com>
2023-03-31 17:37:49 +0900

    fix2

--- pages/projectdetail/[id].tsx
+++ pages/projectdetail/[id].tsx
@@ -1,38 +1,46 @@
-import { Client } from "@notionhq/client";
+// import { Client } from "@notionhq/client";
+// import React from "react";
+
+// type typeParams = {
+//   params: {
+//     id: string;
+//   };
+// };
+
+// const Projectdetail = () => {
+//   return (
+//     <>
+//       <div></div>
+//     </>
+//   );
+// };
+
+// export const getStaticPaths = async () => {
+//   const notion = new Client({
+//     auth: process.env.NOTION_SECRET,
+//   });
+
+//   const paths = ["t", "e", "s", "t"];
+//   //   const data = await notion.blocks.children.list({
+//   //     block_id: process.env.PAGE_ID,
+//   //   });
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
+// export const getStaticProps = async ({ params: { id } }: typeParams) => {
+//   return {
+//     props: {},
+//   };
+// };
+
+// export default Projectdetail;
+
 import React from "react";
 
-type typeParams = {
-  params: {
-    id: string;
-  };
-};
-
 const Projectdetail = () => {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-};
-
-export const getStaticPaths = async () => {
-  const notion = new Client({
-    auth: process.env.NOTION_SECRET,
-  });
-
-  const paths = ["t", "e", "s", "t"];
-  //   const data = await notion.blocks.children.list({
-  //     block_id: process.env.PAGE_ID,
-  //   });
-  return {
-    paths,
-    fallback: false,
-  };
-};
-export const getStaticProps = async ({ params: { id } }: typeParams) => {
-  return {
-    props: {},
-  };
+  return <div>[id]</div>;
 };
 
 export default Projectdetail;
