e97ea51c8387d7d32b8f06654a248d42f34528c1
InhooPark <park.linhoo@gmail.com>
2023-04-04 21:42:43 +0900

    intro update

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,18 +1,69 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import Style from "@/styles/intro.module.scss";
 
 const Intro = () => {
   const router = useRouter();
+  const [maskimg, setMaskimg] = useState("00.jpg");
+  const [bgcolor, setBgcolor] = useState("EEEAEB");
 
-  setTimeout(() => {
-    router.replace("/about");
-  }, 2000);
+  const color = ["F4BE1D", "399CA3", "F48D20", "57ABAD"];
+  const img = ["cook.gif", "swim.gif", "game.gif", "matrix.gif"];
+
+  function profileOver(quadrant: number) {
+    setBgcolor(color[quadrant - 1]);
+    setMaskimg(img[quadrant - 1]);
+  }
+  function profileLeave(quadrant: number) {
+    setMaskimg("00.jpg");
+    setBgcolor("EEEAEB");
+  }
+  function toAbout() {
+    router.push("/about");
+  }
 
   return (
     <main className={Style.intro_main}>
       <section className={Style.intro_section}>
-        <h1>박인후 포트폴리오</h1>
+        <div className={Style.intro_left_block} style={{ backgroundColor: `#${bgcolor}` }}>
+          <div className={Style.intro_left_block_img_wrap} onMouseEnter={() => profileOver(4)} onMouseLeave={() => profileLeave(4)}>
+            <img src="" alt="profile image program" />
+          </div>
+          <div className={Style.intro_left_block_img_wrap} onMouseEnter={() => profileOver(1)} onMouseLeave={() => profileLeave(1)}>
+            <img src="" alt="profile image cook" />
+          </div>
+          <div className={Style.intro_left_block_img_wrap} onMouseEnter={() => profileOver(3)} onMouseLeave={() => profileLeave(3)}>
+            <img src="" alt="profile image game" />
+          </div>
+          <div className={Style.intro_left_block_img_wrap} onMouseEnter={() => profileOver(2)} onMouseLeave={() => profileLeave(2)}>
+            <img src="" alt="profile image swim" />
+          </div>
+        </div>
+        <div className={Style.intro_right_block} style={{ backgroundColor: `#${bgcolor}` }}>
+          {maskimg === "game.gif" ? (
+            <h1 style={{ backgroundImage: `url("/img/testimg/${maskimg}")`, backgroundPositionY: "-2.5vh" }}>
+              {"PIH's"} <br />
+              {"Portfolio"}
+              <br />
+              <div className={Style.intro_right_block_toAbout}>
+                <div className={Style.intro_toAbout_wrap} onClick={toAbout}>
+                  <p>{"➡"}</p>
+                </div>
+              </div>
+            </h1>
+          ) : (
+            <h1 style={{ backgroundImage: `url("/img/testimg/${maskimg}")` }}>
+              {"PIH's"} <br />
+              {"Portfolio"}
+              <br />
+              <div className={Style.intro_right_block_toAbout}>
+                <div className={Style.intro_toAbout_wrap} onClick={toAbout}>
+                  <p>{"➡"}</p>
+                </div>
+              </div>
+            </h1>
+          )}
+        </div>
       </section>
     </main>
   );
