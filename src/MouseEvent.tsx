114da74534aa5f79ed18a5800efc324012634d5e
InhooPark <park.linhoo@gmail.com>
2023-04-19 14:03:21 +0900

    mouseE fix

--- src/MouseEvent.tsx
+++ src/MouseEvent.tsx
@@ -3,6 +3,7 @@ import Cursor from "./Cursor";
 
 const MouseEvent = () => {
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
+  const [inW, setInW] = useState(1920);
 
   useEffect(() => {
     function trackMouse(e: MouseEvent) {
@@ -12,11 +13,16 @@ const MouseEvent = () => {
     return () => window.removeEventListener("mousemove", trackMouse);
   }, []);
 
-  return (
-    <div>
-      <Cursor x={mouse.x} y={mouse.y}></Cursor>
-    </div>
-  );
+  useEffect(() => {
+    setInW(window.innerWidth);
+    function resizeFunc() {
+      setInW(window.innerWidth);
+    }
+    window.addEventListener("resize", resizeFunc);
+    return () => window.removeEventListener("resize", resizeFunc);
+  });
+
+  return <div>{inW > 1024 ? <Cursor x={mouse.x} y={mouse.y}></Cursor> : <></>}</div>;
 };
 
 export default MouseEvent;
